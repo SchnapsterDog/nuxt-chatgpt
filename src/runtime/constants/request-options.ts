def9dbcb0c264fc13fa055738f033f45fd5caaac
SchnapsterDog <olivertrajceski@yahoo.com>
2023-04-07 10:43:15 +0200

    perf: created constants folder, added defaultOptions for api request

---
+++ src/runtime/constants/request-options.ts
@@ -0,0 +1,8 @@
+export const defaultOptions = {
+  model: "text-davinci-003",
+  temperature: 0.5,
+  max_tokens: 2048,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0
+}
--- src/runtime/server/api/openai.ts
+++ src/runtime/server/api/openai.ts
@@ -1,27 +1,35 @@
 import { createError, defineEventHandler, readBody } from 'h3'
 import { Configuration, OpenAIApi } from "openai";
+import { defaultOptions } from '../../constants/request-options';
 
 import { useRuntimeConfig } from '#imports'
 
 export default defineEventHandler(async (event) => {
+  // get the data that comes from the request
   const prompt = await readBody(event)
 
+  // throw an error if the apiKey is not set
+  if (!useRuntimeConfig().chatgpt.apiKey) {
+    throw createError({
+      statusCode: 403,
+      message: 'Missing OpenAI API Key',
+    })
+  }
+
+  // set-up configuration object and apiKEY
   const configuration = new Configuration({
     apiKey: useRuntimeConfig().chatgpt.apiKey
   });
 
+  // init new instance of openai and pass configuration into it
   const openai = new OpenAIApi(configuration);
 
+  // merge the prompt object and default options
+  const requestOptions = { prompt, ...defaultOptions }
+
   try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      temperature: 0.5,
-      max_tokens: 2048,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0
-    });
+    // send the request and prepare the response
+    const response = await openai.createCompletion(requestOptions);
     return response.data.choices[0].text?.slice(2)
   } catch (error) {
     throw createError({
