97d89b2a6992b8136affde56cb370d0208cf160b
ArifYasinKavdir <arifyasinkavdir@gmail.com>
2024-01-03 13:41:02 +0300

    chore(release): v0.2.2

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   chatgpt: {
-    apiKey: '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',
+    apiKey: 'your-api-key',
     isEnabled: true
   }
 })
