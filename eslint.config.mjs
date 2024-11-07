996c113f123c2c006945b0c0396c6792fac9926e
Bit0r <nie_wang@outlook.com>
2024-11-07 19:23:17 +0800

    feat: update version to 0.2.6, update ESLint config.

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default [{
+  ignores: ["**/dist", "**/node_modules", "**/.nuxt"],
+}, ...compat.extends("@nuxt/eslint-config")]
